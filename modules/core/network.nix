a66f51a4860a0821876fa9c9117e5655698db893
Caspar Adriani <cas@pluxbox.com>
2024-10-17 23:41:42 +0200

    added tailscale

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -1,4 +1,4 @@
-{ pkgs, host, ... }: 
+{ pkgs, host, config, ... }:
 {
   networking = {
     hostName = "${host}";
@@ -6,10 +6,14 @@
     nameservers = [ "8.8.8.8" "8.8.4.4" "1.1.1.1" ];
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 22 80 443 59010 59011 ];
-      allowedUDPPorts = [ 59010 59011 ];
+      trustedInterfaces = [ "tailscale0" ];
+      allowedTCPPorts = [ 22 8080 ];
+      allowedUDPPorts = [ config.services.tailscale.port ];
     };
   };
+
+  services.tailscale.enable = true;
+  services.tailscale.useRoutingFeatures = "both";
 
   environment.systemPackages = with pkgs; [
     networkmanagerapplet
