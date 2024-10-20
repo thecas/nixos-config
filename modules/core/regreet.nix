19308a8bec4581941dd709b13bacaca26824a2db
Caspar Adriani <cas@pluxbox.com>
2024-10-20 18:05:49 +0200

    added config

--- modules/core/regreet.nix
+++ modules/core/regreet.nix
@@ -2,8 +2,12 @@
 {
     programs.regreet = {
         enable = true;
-        # settings = {
-        #     GTK.
-        # }
+        settings = {
+            background.path = "/usr/share/backgrounds/greeter.jpg";
+            GTK = {
+                application_prefer_dark_theme = true;
+
+            };
+        };
     };
 }
