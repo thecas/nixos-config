75ba3bd29e462b619402868b70d55e5b14a507be
Caspar Adriani <cas@pluxbox.com>
2024-10-28 22:39:50 +0100

    update and fixed laptop slooooooow speed

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -22,7 +22,7 @@
 
 
   services = {
-    power-profiles-daemon.enable = true;
+    power-profiles-daemon.enable = false;
 
     upower = {
       enable = true;
@@ -32,12 +32,19 @@
       criticalPowerAction = "PowerOff";
     };
 
+    tlp.enable = true;
     tlp.settings = {
+
       CPU_ENERGY_PERF_POLICY_ON_AC = "power";
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
       CPU_BOOST_ON_AC = 1;
       CPU_BOOST_ON_BAT = 1;
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
 
       CPU_HWP_DYN_BOOST_ON_AC = 1;
       CPU_HWP_DYN_BOOST_ON_BAT = 1;
