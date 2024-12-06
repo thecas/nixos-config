de3f50df37276559486fe06c7cff2e407cf0db1f
Caspar Adriani <cas@pluxbox.com>
2024-12-06 23:30:44 +0100

    tried to fix the multi-monitor problem

--- modules/core/regreet.nix
+++ modules/core/regreet.nix
@@ -2,7 +2,11 @@
 {
     programs.regreet = {
         enable = true;
-        settings = {
+
+	#only one monitor
+        #cageArgs = [ "-s" "-m" "last" ];
+        
+	settings = {
             background.path = "/usr/share/backgrounds/greeter.jpg";
             GTK = {
                 application_prefer_dark_theme = true;
