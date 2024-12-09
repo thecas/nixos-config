27f63243239321fbcb4dd479d262bae4725a4154
Caspar Adriani <cas@pluxbox.com>
2024-12-09 18:33:52 +0100

    added cups

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -5,9 +5,17 @@
     gnome.gnome-keyring.enable = true;
     dbus.enable = true;
     fstrim.enable = true;
+    printing.enable = true;
   };
   services.logind.extraConfig = ''
     # don’t shutdown when power button is short-pressed
     HandlePowerKey=ignore
   '';
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+
 }
