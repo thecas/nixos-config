febe53d2c9c20331f08bc05d6d83aa4d82c79e07
Caspar Adriani <cas@pluxbox.com>
2024-12-23 08:35:57 +0100

    made laptop more faster

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -45,7 +45,7 @@
         CPU_MIN_PERF_ON_AC = 0;
         CPU_MAX_PERF_ON_AC = 100;
         CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 20;
+        CPU_MAX_PERF_ON_BAT = 60;
 
         CPU_HWP_DYN_BOOST_ON_AC = 1;
         CPU_HWP_DYN_BOOST_ON_BAT = 1;
